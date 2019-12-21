4318cc72bb10532c06c1348d4289c7dd6916f7e4
Charlotte Beale <charlottebeale@zipcoders-MacBook-Pro-6.local>
2019-12-21 14:59:47 -0500

    Complete

--- Crypto/src/ROT13.java
+++ Crypto/src/ROT13.java
@@ -1,65 +1,73 @@
+package src
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 import static java.lang.Character.isLowerCase;
 import static java.lang.Character.isUpperCase;
 import static java.lang.Character.toLowerCase;
 
 public class ROT13  {
-private final startUpperCase;
-private String lowercaseStart = "abcdefghijqlmnpqrstuvwxyz";
-private String uppercaseStart = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-protected String startLower;
-protected String startUpper;
-protected String registerLower;
-protected String registerUpper;
-private boolean symmetric = false;
-
+    private int shift;
     ROT13(Character cs, Character cf) {
-        if(toLowerCase(cs) == 'a' && toLowerCase(cf) == 'n') symmetric = true;
-
-        startUpper = ROT13.rotate(uppercaseStart, Character.toUpperCase(cs));
-        startLower = ROT13.rotate(lowercaseStart, Character.toLowerCase(cs));
-
-        registerUpper = ROT13.rotate(uppercaseStart, Character.toUpperCase(cf));
-        registerLower = ROT13.rotate(lowercaseStart, Character.toLowerCase(cf));
-
-        System.out.println(startUpper);
-        System.out.println(registerUpper);
-        System.out.println(startLower);
-        System.out.println(registerLower);
-
+        this.shift = cs - cf;
+    }
 
     ROT13() {
-    this.('a', 'n');
+        shift = 13;
     }
 
-    public String crypt(String text) throws UnsupportedOperationException {
-        if (this.symmetric != true) throw new Unsupported OperationalException();
-        return substitute(text, startUpper, registerUpper, startLower, registerLower);
+
+    public static String crypt(String text) throws UnsupportedOperationException {
+        StringBuilder sb = new StringBuilder();
+        char [] chars = text.toCharArray();
+        for (int i = 0; i <chars.length ; i++) {
+            char c = text.charAt(i);
+            if(c >= 'a' && c <= 'm') {
+                c += 13;
+            }else if  (c >= 'A' && c <= 'M') {
+                c += 13;
+            } else if  (c >= 'n' && c <= 'z') {
+                c -= 13;
+            } else if  (c >= 'N' && c <= 'Z') {
+                c -= 13;
+            }
+            sb.append(c);
+        }
+        return sb.toString();
     }
 
-    public String encrypt(String text) {
-        return substitute(text,startUpper, registerUpper, startLower, registerLower);
+    public static String encrypt(String text) {
+        return crypt(text);
     }
 
     public String decrypt(String text) {
-        return substitute(text,startUpper, registerUpper, startLower, registerLower);
+        return crypt(text);
     }
 
     public static String rotate(String s, Character c) {
+        //this method will rotate the given String
+        //by the character placement
+        String sub = s.substring(0,s.indexOf(c));
+        String sub1 = s.substring(s.indexOf(c));
+        //then concat the two substrings.
+        return sub1.concat(sub);
+    }
+    public static String textFile(String filename){
+        StringBuilder sb = new StringBuilder();
 
-        return "";
+        File file = new File(filename);
+        try(Scanner sc = new Scanner(file)) {
+            while(sc.hasNextLine()){
+                String line = sc.nextLine();
+                String xored = encrypt(line);
+                sb.append(xored+"\n");
+            }
+            sb.deleteCharAt(sb.length()-1);
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        return sb.toString();
     }
 
-    private String substitute(String text, String startUpper, String registerUpper, String startLower, String registerLower){
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < text.length(); i++){
-            Character ch = text.charAt(i);
-            Integer position = 0;
-            if (isUpperCase(ch)) {
-                position = startUpper.indexOf(ch);
-                sb.append(registerUpper.charAt(i));
-                }
-            }
-
-            return null;
-        }
 }
